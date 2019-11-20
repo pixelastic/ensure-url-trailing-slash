6891345cf1743f568e66aad84a440bf474eec983
Pixelastic <tim@pixelastic.com>
2019-11-20 16:26:16 +0100

    feat(build): Add spinner steps on pre-build

--- scripts/pre-build.js
+++ scripts/pre-build.js
@@ -3,8 +3,10 @@ import path from 'path';
 import terser from 'terser';
 
 (async () => {
+  const spinner = firost.spinner();
   // Compile the method to ES2015
   const tmpDir = path.resolve('./tmp/pre-build');
+  spinner.tick('Compile source to ES2015');
   await firost.shell(`yarn run aberlaas build --out-dir=${tmpDir}`);
 
   // Get the source of the method
@@ -12,6 +14,7 @@ import terser from 'terser';
   const source = method.toString();
 
   // Minify it
+  spinner.tick('Minify it');
   const minified = terser.minify(source).code;
 
   // Keep only the method body
@@ -19,7 +22,14 @@ import terser from 'terser';
   const { content } = minified.match(regexp).groups;
 
   // Save it as source.js
+  spinner.tick('Save it as string');
   const fileContent = `export default '${content}'`;
   const filepath = './lib/source.js';
   await firost.write(fileContent, filepath);
+
+  // Prettify it
+  spinner.tick('Prettify it');
+  await firost.shell('yarn run aberlaas lint --fix ./lib/source.js');
+
+  spinner.success('Pre-build finished');
 })();
